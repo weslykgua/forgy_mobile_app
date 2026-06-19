b586dac2f5a7b05c3bfab7cb0cda0e29c66b27da
Wesly Cagua <weslykgua@gmail.com>
2026-06-19 03:41:25 -0400

    fix: change getHeaders function export to local scope for better encapsulation

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -20,7 +20,7 @@ const { userName, userEmail, loadProfileData, logout } = useProfile()
 
 // Configuración local para acceso a la API para no modificar useProfile.ts
 const API_URL = import.meta.env.VITE_API_URL
-export const getHeaders = () => ({
+const getHeaders = () => ({
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('token')}`
 })
